38a0945d7265f3ec861a46be5d963c1c760ffc29
gookudog321 <gookudog321@hotmail.com>
2020-10-23 21:38:48 +0700

    Edit page

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Carditem from './Carditem'
 import './Cards.css'
+import img5 from '../img/img-5.jpg'
+import img6 from '../img/img-6.jpg'
+import img4 from '../img/img-4.jpg'
+import img8 from '../img/img-8.jpg'
+import img9 from '../img/img-9.jpg'
 
-function Cards() {
-    return (
-        <div className="cards">
+
+export default class Cards extends Component {
+    render() {
+        return (
+            <>
+                 <div className="cards">
             <h1>CHECK out these EPIC Destinations!</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <Carditem 
-                        src="img/img-9.jpg"
+                        src={img5}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Advanture"
                         path="/services"
                         />
                         <Carditem 
-                        src="img/img-2.jpg"
+                        src={img6}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Advanture"
                         path="/services"
@@ -25,19 +33,19 @@ function Cards() {
 
                     <ul className="cards__items">
                         <Carditem 
-                        src="img/img-3.jpg"
+                        src={img9}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Advanture"
                         path="/services"
                         />
                         <Carditem 
-                        src="img/img-4.jpg"
+                        src={img8}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Advanture"
                         path="/services"
                         />
                         <Carditem 
-                        src="img/img-8.jpg"
+                        src={img4}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Advanture"
                         path="/services"
@@ -46,7 +54,63 @@ function Cards() {
                 </div>
             </div>
         </div>
-    )
+            </>
+        )
+    }
 }
 
-export default Cards
+
+
+// import React, { Component } from 'react'
+// import Carditem from './Carditem'
+// import './Cards.css'
+// import img3 from '../img/img/img-3.jpg'
+// export default class Cards extends Component {
+//     return (){
+//         return(
+//         <div className="cards">
+//             <h1>CHECK out these EPIC Destinations!</h1>
+//             <div className="cards__container">
+//                 <div className="cards__wrapper">
+//                     <ul className="cards__items">
+//                         <Carditem 
+//                         src="img/img-3.jpg"
+//                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
+//                         label="Advanture"
+//                         path="/services"
+//                         />
+//                         <Carditem 
+//                         src="img/img-3.jpg"
+//                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
+//                         label="Advanture"
+//                         path="/services"
+//                         />
+//                     </ul>
+
+//                     <ul className="cards__items">
+//                         <Carditem 
+//                         src="img/img-3.jpg"
+//                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
+//                         label="Advanture"
+//                         path="/services"
+//                         />
+//                         <Carditem 
+//                         src="img/img-4.jpg"
+//                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
+//                         label="Advanture"
+//                         path="/services"
+//                         />
+//                         <Carditem 
+//                         src="img/img-8.jpg"
+//                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
+//                         label="Advanture"
+//                         path="/services"
+//                         />
+//                     </ul>
+//                 </div>
+//             </div>
+//         </div>
+//         )
+// }
+// }
+
