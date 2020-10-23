33dc3b42d67591838064e002720a289aec44c49d
gookudog321 <gookudog321@hotmail.com>
2020-10-23 21:06:47 +0700

    Updates

---
+++ img/Commitment.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Commitment extends Component {
+    render() {
+        return (
+            <section className="content container">
+                 <h3>OUR COMMITMENT</h3>
+                 <p className="p2" />
+                <div className="content-grid">
+                    <h2>THE HOME OF  FRESHNESS</h2>
+                    <div>
+                    Two exquisite objection delighted deficient yet its contained. Cordial because are account evident its subject but eat. Can properly followed learning prepared you doubtful yet him. Over many our good lady feet ask that. Expenses own moderate day fat trifling stronger sir domestic feelings. <br /><br />Itself at be answer always exeter up do. Though or my plenty uneasy do. Friendship so considered remarkably be to sentiments. Offered mention greater fifteen one promise because nor. Why denoting speaking fat indulged saw dwelling raillery. 
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
---
+++ precache-manifest.daa6bce489f700c737442a776b0c3f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef07f55f1724dc0e2327590217e3e6e",
+    "url": "/work_shop_two/index.html"
+  },
+  {
+    "revision": "fe7e9be090b193846420",
+    "url": "/work_shop_two/static/css/main.9a630170.chunk.css"
+  },
+  {
+    "revision": "cf4ce8807e17991a1dad",
+    "url": "/work_shop_two/static/js/2.c9bbeee1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/work_shop_two/static/js/2.c9bbeee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7e9be090b193846420",
+    "url": "/work_shop_two/static/js/main.59cbd15e.chunk.js"
+  },
+  {
+    "revision": "beee33b24bc71ca0d924",
+    "url": "/work_shop_two/static/js/runtime-main.e2c70082.js"
+  }
+]);
